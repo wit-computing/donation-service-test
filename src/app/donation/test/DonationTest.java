5eb4ec3b845d21a4d8a12dc2d0d3c6aaf17b2456
Eamonn de Leastar <edeleastar@wit.ie>
2015-11-10 16:56:42 +0000

    donation tests

--- src/app/donation/test/DonationTest.java
+++ src/app/donation/test/DonationTest.java
@@ -2,7 +2,6 @@ package app.donation.test;
 
 import static org.junit.Assert.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.After;
@@ -11,69 +10,76 @@ import org.junit.Test;
 
 import app.donation.main.DonationServiceAPI;
 import app.donation.model.Donation;
+import app.donation.model.Donor;
 
 public class DonationTest
 {
-  static Donation donationArray [] = 
-  { 
-    new Donation (23,   "visa"),
-    new Donation (3,    "paypal"),
-    new Donation (423,  "cashl"),
-    new Donation (11,   "paypal")
-  };
-  List <Donation> donationList = new ArrayList<>();	
-	
+  private Donor marge =  new Donor ("marge",  "simpson", "homer@simpson.com",  "secret");
+  
   private DonationServiceAPI donationServiceAPI = new DonationServiceAPI();
-	
+  
   @Before
   public void setup() throws Exception
   { 
-    for (Donation donation : donationArray)
-    {
-      Donation returned = donationServiceAPI.createDonation(donation);
-      donationList.add(returned);
-    }
+    marge = donationServiceAPI.createDonor(marge);
   }
   
   @After
   public void teardown() throws Exception
   {
-	donationServiceAPI.deleteAllDonations();  
-  }  
+    donationServiceAPI.deleteDonor(marge.id);
+  }
   
   @Test
-  public void testCreate () throws Exception
+  public void testCreateDonation () throws Exception
   {
-    assertEquals (donationArray.length, donationList.size());
-    for (int i=0; i<donationArray.length; i++)
-    {
-      assertEquals(donationList.get(i), donationArray[i]);
-    }
+    Donation donation = new Donation (123, "cash");
+    Donation returnedDonation = donationServiceAPI.createDonation(marge.id, donation);
+    assertEquals (donation, returnedDonation);
+    
+    donationServiceAPI.deleteDonation(marge.id, returnedDonation.id);
   }
+  
+  @Test
+  public void testCreateDonations () throws Exception
+  {
+    Donation donation1 = new Donation (123, "cash");
+    Donation donation2 = new Donation (450, "cash");
+    Donation donation3 = new Donation (43,  "paypal");
+    
+    Donation returnedDonation1 = donationServiceAPI.createDonation(marge.id, donation1);
+    Donation returnedDonation2 = donationServiceAPI.createDonation(marge.id, donation2);
+    Donation returnedDonation3 = donationServiceAPI.createDonation(marge.id, donation3);
+    
+    assertEquals(donation1, returnedDonation1);
+    assertEquals(donation2, returnedDonation2);
+    assertEquals(donation3, returnedDonation3);
 
-  @Test
-  public void testList() throws Exception
-  {
-    List<Donation> list = donationServiceAPI.getAllDonations();
-    assertTrue (list.containsAll(donationList));
+    donationServiceAPI.deleteDonation(marge.id, returnedDonation1.id);
+    donationServiceAPI.deleteDonation(marge.id, returnedDonation2.id);    
+    donationServiceAPI.deleteDonation(marge.id, returnedDonation3.id);
   }
   
   @Test
-  public void testDelete () throws Exception
+  public void testListDonations () throws Exception
   {
-    List<Donation> list1 = donationServiceAPI.getAllDonations();
+    Donation donation1 = new Donation (123, "cash");
+    Donation donation2 = new Donation (450, "cash");
+    Donation donation3 = new Donation (43,  "paypal");
     
-    Donation testdonation = new Donation(1234, "debit");
-    Donation returnedDonation = donationServiceAPI.createDonation(testdonation);
+    donationServiceAPI.createDonation(marge.id, donation1);
+    donationServiceAPI.createDonation(marge.id, donation2);
+    donationServiceAPI.createDonation(marge.id, donation3);
     
-    List<Donation> list2 = donationServiceAPI.getAllDonations();
-    assertEquals (list1.size()+1, list2.size());
+    List<Donation> donations = donationServiceAPI.getDonations(marge.id);
+    assertEquals (3, donations.size());
     
-    int code = donationServiceAPI.deleteDonation(returnedDonation.id);
-    assertEquals (200, code);
-    
-    List<Donation> list3 = donationServiceAPI.getAllDonations();
-    assertEquals (list1.size(), list3.size());
+    assertTrue(donations.contains(donation1));
+    assertTrue(donations.contains(donation2));
+    assertTrue(donations.contains(donation3));
+
+    donationServiceAPI.deleteDonation(marge.id, donations.get(0).id);
+    donationServiceAPI.deleteDonation(marge.id, donations.get(1).id);    
+    donationServiceAPI.deleteDonation(marge.id, donations.get(2).id);
   }
-  
 }
